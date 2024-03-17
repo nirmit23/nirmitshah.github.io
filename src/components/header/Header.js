d5c83b5732404dae570a946f5ed11a5426fd4d08
nirmit231999 <145090372+nirmit231999@users.noreply.github.com>
2024-03-16 21:47:51 -0400

    testing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -17,7 +17,7 @@ const onMouseOut = (event) => {
 
 class Header extends Component {
   render() {
-    const theme = this.props.theme;
+    const theme = this.props.theme; 
     const link = settings.isSplash ? "/splash" : "home";
     return (
       <Fade top duration={1000} distance="20px">
